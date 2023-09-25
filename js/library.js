0496c5ee7a71269069addef73c56e4e306d4a548
남동훈 <seye1479@gmail.com>
2023-09-25 11:35:21 +0900

    feat: add about.html

---
+++ js/library.js
@@ -0,0 +1,6 @@
+// Function to change image with smooth transition
+const changeImage = (newSrc) => {
+  const imageElement = document.getElementById("circle-image");
+  imageElement.src = newSrc;
+      
+};
