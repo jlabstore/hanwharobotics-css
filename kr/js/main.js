b16573bd0bd1b051b93bd792acb21557ef76a932
남동훈 <seye1479@gmail.com>
2023-11-15 00:33:04 +0900

    - 메인 애니메이션 추가 - 헤더 header_inner 태그 추가

--- kr/js/main.js
+++ kr/js/main.js
@@ -1,5 +1,7 @@
 
 let resizeTimeout = null;
+let lastScrollPos = 0;
+const $header = $("#header");
 
 function hideHeader() {
   let lastScrollPos = 0;
@@ -41,11 +43,50 @@ function pcBoxPositionUp() {
   });
 }
 
-$(window).on("load resize", function (e) {
+$(window).on("resize", function (e) {
   hideHeader();
-
 
   if ($(this).width() >= 743) {
     pcBoxPositionUp();
   }
+});
+
+$(window).on("load", function (e) {
+  hideHeader();
+
+  if ($(this).width() >= 743) {
+    pcBoxPositionUp();
+  }
+
+  document.querySelectorAll('.main .section1 .contents').forEach((section, index, array) => {
+    // Pin the section
+    gsap.to(section, {
+      scrollTrigger: {
+        trigger: section,
+        start: "top top",
+        end: "bottom top",
+        pin: true,
+        scrub: true, // Use scrub to smoothly animate the changes over the scroll duration
+      }
+    });
+  
+    // Animate the .title and .txt within each section
+    gsap.fromTo(section.querySelectorAll('.title, .txt'), {
+      y: 50, // start 50 pixels below their final position
+      opacity: 0 // start fully transparent
+    }, {
+      y: 0, // end at their final position
+      opacity: 1, // fade in to fully opaque
+      duration: 1, // duration of the fade in effect
+      scrollTrigger: {
+        trigger: section,
+        start: "top center+=100", // start the fade in a little after the top of the section hits the center
+        end: "bottom center", // end the fade in when the bottom of the section hits the center
+        scrub: true // animate smoothly in conjunction with the scroll
+      },
+      stagger: 0.1 // stagger the start of the animations for each title and txt
+    });
+  });
+
+  
 });
